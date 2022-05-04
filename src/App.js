8e5229d1465fe04abe097a8d02a9ae3730f88d82
mikamada <mikamada17@gmail.com>
2022-05-04 19:20:49 +0700

    feat: add component navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import Home from './pages/Home';
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
+	return <Home />;
 }
 
 export default App;
